67fac9be3a168496fe43f1d33988d657e98cd5d9
Tyler Hampton <howdoicomputer@fastmail.com>
2023-10-29 12:49:10 -0700

    Documentation

---
+++ README.md
@@ -0,0 +1,60 @@
+# aws-cdktf-example
+
+This project is a very simple reference architecture in AWS using the TypeScript version of [cdktf](https://github.com/hashicorp/terraform-cdk).
+
+## Environment
+
+Within this reference, an environment is a logical construct that exists as a VPC with an EKS cluster within it. The cidrs are for the VPCs are `/16` and each subnet is a `/24` with three private subnets and three public subnets. Each subnet public/private pair corresponds to an availability zone. The private subnets are where k8s worker nodes are deployed to and the public subnets are where load balancers live.
+
+The difference between a private and public subnet is whether or not there is a route to an internet gateway defined. Most of the setup around subnets are handled by the [VPC](https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest) terraform module.
+
+Code wise, an environment is handled by the `Environment` class and configured by the `EnvironmentOptions` interface.
+
+Example:
+
+``` typescript
+    const dev = new Environment(this, "development", {
+      env: "development",
+      cidr: "10.1.0.0/16",
+      privateSubnets: ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"],
+      publicSubnets: ["10.1.4.0/24", "10.1.5.0/24", "10.1.6.0/24"],
+      enableNatGateway: true,
+      singleNatGateway: true,
+      enableDnsHostnames: true,
+    });
+```
+
+## EKS Makeup
+
+* Karpenter - the ability to autoscale the EKS cluster for unschedudulable resources
+* ArgoCD - GitOps tool for syncing k8s state to each EKS cluster
+* AWS Load Balancer Controller Add-on - adds the ability for k8s workloads to use an AWS load balancer type as an ingress
+
+## Cost Profile
+
+This codebase tries not to incur unnecessary costs but they are inevitable.
+
+For example,
+
+* AWS NAT Gateway - charged per-hour it exists and the data coming through it
+* The Karpenter nodes are spot-bid and can increase cost if you give a reason to scale
+
+## Potential Improvements
+
+### Use AWS Accounts as Env
+
+AWS managed resources are global to an account. This codebase uses VPCs and EKS to represent a logical environment. However, this boundary only recovers a slice of resources; security groups, buckets, etc would need additional controls to create an environmental boundaries.
+
+Using AWS accounts as environment silos allows you to create silos for AWS global resources and is actually the recommended practice by AWS.
+
+### Shell Access
+
+Currently there is no bastion host or an administrator subnet for it to exist in. A good way of solving instance connectivity in AWS is to use [session manager](https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager.html) to create shells for remote access. This is generally superior to SSH as it removes the need for opening up port 22, allows you to use IAM policies for authorization, and also allows auditing by shipping access trail logs to CloudTrail and session logs to CloudWatch.
+
+### Access to AWS Hosted Services
+
+This codebase does not set up any of the prerequisites for a VPN. One potential solution is to use a [Tailscale subnet router](https://tailscale.com/kb/1296/aws-reference-architecture/#ip-based-connectivity-with-subnet-router) to connect a Tailscale network to your AWS network. This would allow any Tailscale-enabled workstation to, say, query RDS directly.
+
+### Inter-VPC Communication
+
+This codebase assumes that environments are hermetic. There are multiple ways to get VPCs to talk to each other: VPC peering and Transit Gateway. There are pros and cons to both.
--- environment/index.ts
+++ environment/index.ts
@@ -7,75 +7,75 @@ import { DataAwsEksClusterAuth } from "../.gen/providers/aws/data-aws-eks-cluste
 import { HelmProvider } from "../.gen/providers/helm/provider";
 
 interface EnvironmentOptions {
-    env: "development" | "staging" | "production";
-    cidr: string;
-    privateSubnets: string[];
-    publicSubnets: string[];
-    enableNatGateway: boolean;
-    singleNatGateway: boolean;
-    enableDnsHostnames: boolean;
+  env: "development" | "staging" | "production";
+  cidr: string;
+  privateSubnets: string[];
+  publicSubnets: string[];
+  enableNatGateway: boolean;
+  singleNatGateway: boolean;
+  enableDnsHostnames: boolean;
 }
 
 export class Environment extends Construct {
-    public vpc: Vpc;
-    public eks: Eks;
-    public eksAuth: DataAwsEksClusterAuth;
+  public vpc: Vpc;
+  public eks: Eks;
+  public eksAuth: DataAwsEksClusterAuth;
 
-    constructor(scope: Construct, name: string, options: EnvironmentOptions) {
-        super(scope, name);
+  constructor(scope: Construct, name: string, options: EnvironmentOptions) {
+    super(scope, name);
 
-        const allAwsAvailabilityZones = new DataAwsAvailabilityZones(
-            this,
-            "all-availability-zones",
-            {}
-        ).names;
+    const allAwsAvailabilityZones = new DataAwsAvailabilityZones(
+      this,
+      "all-availability-zones",
+      {}
+    ).names;
 
-        this.vpc = new Vpc(this, options.env, {
-            name: options.env,
-            cidr: options.cidr,
-            azs: allAwsAvailabilityZones,
-            privateSubnets: options.privateSubnets,
-            publicSubnets: options.publicSubnets,
-            enableNatGateway: options.enableNatGateway,
-            singleNatGateway: options.singleNatGateway,
-            enableDnsHostnames: options.enableDnsHostnames,
+    this.vpc = new Vpc(this, options.env, {
+      name: options.env,
+      cidr: options.cidr,
+      azs: allAwsAvailabilityZones,
+      privateSubnets: options.privateSubnets,
+      publicSubnets: options.publicSubnets,
+      enableNatGateway: options.enableNatGateway,
+      singleNatGateway: options.singleNatGateway,
+      enableDnsHostnames: options.enableDnsHostnames,
 
-            tags: {
-                ["env"]: options.env,
-            },
-            publicSubnetTags: {
-                "kubernetes.io/role/elb": "1",
-            },
-            privateSubnetTags: {
-                "kubernetes.io/role/internal-elb": "1",
-            },
-        });
+      tags: {
+        ["env"]: options.env,
+      },
+      publicSubnetTags: {
+        "kubernetes.io/role/elb": "1",
+      },
+      privateSubnetTags: {
+        "kubernetes.io/role/internal-elb": "1",
+      },
+    });
 
-        this.eks = new Eks(this, "eks", {
-            clusterName: options.env,
-            clusterVersion: "1.28",
-            clusterEndpointPublicAccess: true,
-            clusterEndpointPublicAccessCidrs: ["76.132.5.161/32"],
-            subnetIds: Fn.tolist(this.vpc.privateSubnetsOutput),
-            vpcId: this.vpc.vpcIdOutput,
-            eksManagedNodeGroupDefaults: {
-                intanceTypes: "t2.micro",
-            },
-            eksManagedNodeGroups: {
-                green: {
-                    minSize: 1,
-                    maxSize: 2,
-                    desiredSize: 1,
-                    instanceTypes: ["t2.micro"],
-                },
-            },
-            tags: {
-                environment: options.env,
-            },
-        });
+    this.eks = new Eks(this, "eks", {
+      clusterName: options.env,
+      clusterVersion: "1.28",
+      clusterEndpointPublicAccess: true,
+      clusterEndpointPublicAccessCidrs: ["76.132.5.161/32"],
+      subnetIds: Fn.tolist(this.vpc.privateSubnetsOutput),
+      vpcId: this.vpc.vpcIdOutput,
+      eksManagedNodeGroupDefaults: {
+        intanceTypes: "t2.micro",
+      },
+      eksManagedNodeGroups: {
+        green: {
+          minSize: 1,
+          maxSize: 2,
+          desiredSize: 1,
+          instanceTypes: ["t2.micro"],
+        },
+      },
+      tags: {
+        environment: options.env,
+      },
+    });
 
-        this.eksAuth = new DataAwsEksClusterAuth(this, "eks-auth", {
-            name: this.eks.clusterNameOutput,
-        });
-    }
+    this.eksAuth = new DataAwsEksClusterAuth(this, "eks-auth", {
+      name: this.eks.clusterNameOutput,
+    });
+  }
 }
